db74d2d36d45fb9e323e9452ce7a30d5acddf54c
Ayovert <64878932+Ayovert@users.noreply.github.com>
2023-08-23 21:02:25 +0100

    fixed Timer

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,86 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import './style.scss';
 
-const downTime = +new Date().getTime() + 1 * 60 * 1000;
+const downTimeX = (25*60*1000);
 export default function App() {
-  // const [currentTime, setCurrentTime] = useState();
+  const [timerId, setTimerId] = useState(0);
+  const [downTime, setDownTime] = useState(downTimeX);
+
 
   const [timeLeft, setTimeLeft] = useState({
-    minutes: Math.floor((1000 % (1000 * 60 * 60)) / (1000 * 60)),
-    seconds: Math.floor((1000 % (1000 * 60)) / 1000),
+    minutes: 25,
+    seconds: 0
   });
 
   const calculateTimeLeft = () => {
-    const currentTime = +new Date().getTime();
-    const diff = downTime - currentTime;
+    const now = 1000;
+    const diff = downTime - 1000;
 
-    //60 - +(new Date().getSeconds() ===0 ? 60 :new Date().getSeconds())
+    //console.log((diff % (1000 * 60 * 60)) / (1000 * 60))
+
+   /*let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+   let seconds = Math.floor((diff%(1000*60)) / 1000);*/
+
+   let minutes = Math.floor((diff / 1000 / 60) % 60);
+   let seconds = Math.floor((diff / 1000) % 60);
+   
+   let downTimeY = ((minutes + (seconds/60)) * 60 * 1000);
+
+
     setTimeLeft({
-      minutes: Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)),
-      seconds: Math.floor((diff % (1000 * 60)) / 1000),
+      minutes: minutes,
+      seconds: seconds
     });
+
+    setDownTime(downTimeY);
   };
 
-  const startTimer = () => {
-    setTimeout(() => {
+  function startTime(){
+    setTimerId(setTimeout(() => {
       calculateTimeLeft();
-    }, 1000);
-  };
+    }, 1000));
+  }
 
+
+  function stopTimer(){
+    
+    clearTimeout(timerId);
+    setTimerId(0);
+    calculateTimeLeft();
+    
+  }
   useEffect(() => {
     let timer: any;
-    if (timeLeft.seconds > 0 || timeLeft.minutes > 0) {
-      timer = setTimeout(() => {
+    if (timerId > 0) {
+      setTimerId(setTimeout(() => {
         calculateTimeLeft();
-      }, 1000);
+      }, 1000));
     }
 
+   // console.log(timer);
+
+
     return () => {
-      clearTimeout(timer);
+      clearTimeout(timerId);
     };
   }, [timeLeft]);
 
   // console.log(timeLeft);
+//console.log(((23*60*1000) % (1000 * 60 * 60)) / (1000 * 60))
+
+//console.log(downTime);
   return (
     <div className="container">
       <h1>Session 25 Timer</h1>
       <p>Click play to start the clock:)</p>
 
-      <div id="time-left" onClick={() => calculateTimeLeft()}>
+      <div id="time-left" onClick={() => startTime()}>
         {timeLeft.minutes} : {timeLeft.seconds}
+      </div>
+
+
+      <div id="stop" onClick={() => stopTimer()}>
+        STOP
       </div>
     </div>
   );
